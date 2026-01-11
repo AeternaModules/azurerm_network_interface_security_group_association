ca1ace3acddb264f8c0627bf3a89e34ab6c500d0
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:52:29 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_interface_security_group_associations" {
+  description = "All network_interface_security_group_association resources"
+  value       = azurerm_network_interface_security_group_association.network_interface_security_group_associations
+}
+
