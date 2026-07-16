68cb5efee625582947087bc3370380d7efc26d35
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "network_interface_security_group_associations_id" {
   description = "Map of id values across all network_interface_security_group_associations, keyed the same as var.network_interface_security_group_associations"
-  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.id }
+  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "network_interface_security_group_associations_network_interface_id" {
   description = "Map of network_interface_id values across all network_interface_security_group_associations, keyed the same as var.network_interface_security_group_associations"
-  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.network_interface_id }
+  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.network_interface_id if v.network_interface_id != null && length(v.network_interface_id) > 0 }
 }
 output "network_interface_security_group_associations_network_security_group_id" {
   description = "Map of network_security_group_id values across all network_interface_security_group_associations, keyed the same as var.network_interface_security_group_associations"
-  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.network_security_group_id }
+  value       = { for k, v in azurerm_network_interface_security_group_association.network_interface_security_group_associations : k => v.network_security_group_id if v.network_security_group_id != null && length(v.network_security_group_id) > 0 }
 }
 
